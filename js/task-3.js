eea9d06cc01b2fd94eccd8d4cc4979b623f1e379
B0hdan2 <118682191+B0hdan2@users.noreply.github.com>
2024-04-02 13:54:32 +0300

    homework JS-7

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 const listeningForInput = document.querySelector("#name-input");
-listeningForInput.style.cssText =`
+listeningForInput.style.cssText = `
 margin-bottom: 16px;
 border-radius: 4px;
 color: #2e2f4;
@@ -9,7 +9,6 @@ const titleWithGreetings = document.querySelector("#name-output");
 
 const looksAtTheText = (event) => {
   const inputValue = event.target.value.trim();
-  console.log(inputValue);
 
   if (inputValue !== "") {
     return (titleWithGreetings.textContent = inputValue);
--- js/task-6.js
+++ js/task-6.js
@@ -3,32 +3,71 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-
+// дістаю посилання на buttonCreate
 const buttonCreate = document.querySelector("button[data-create]");
 buttonCreate.style.paddingRight = "31.5px";
 buttonCreate.style.paddingLeft = "31.5px";
 buttonCreate.style.marginRight = "16px";
 
+// дістаю посилання на buttonDestroy
 const buttonDestroy = document.querySelector("button[data-destroy]");
 
+// дістаю посилання на input
 const input = document.querySelector('input[type="number"]');
 input.style.width = "150px";
 input.style.borderRadius = "8px";
 input.style.marginRight = "16px";
 
+// дістаю посилання на divBoxes
 const divBoxes = document.querySelector("#boxes");
-// divBoxes.style.cssText = `
-// padding: 32px;
-// border-radius: 8px;`;
 
+// створення div
 const renderDiv = (event) => {
+  // дістаю значення input
   const value = parseInt(input.value);
 
+  // додаю стилі для divBoxes
+  divBoxes.style.cssText = `
+    padding: 32px;
+    border-radius: 8px;
+  `;
+
   if (value >= parseInt(input.min) && value <= parseInt(input.max)) {
-    divBoxes.insertAdjacentHTML("beforeend", `<div></div>`);
-  } else {
-    console.log("no");
+    // видаляю старі div
+    divBoxes.innerHTML = "";
+
+    // створюю div
+    let width = 30;
+    let height = 30;
+
+    const createBoxes = (amount) => {
+      for (let i = 0; i < amount; i++) {
+        const box = document.createElement("div");
+        box.style.backgroundColor = getRandomHexColor();
+        box.style.width = `${width}px`;
+        box.style.height = `${height}px`;
+        divBoxes.appendChild(box);
+
+        width += 10;
+        height += 10;
+      }
+    };
+    // викликаю функцію та передаю аргументом значення input
+    createBoxes(value);
   }
+
+  // видаляю значення input
+  input.value = "";
+};
+
+// видаляю все що знаходиться в divBoxes
+const destroyBoxes = (event) => {
+  divBoxes.innerHTML = "";
+  divBoxes.style.cssText = `
+  padding: 0;
+  border-radius: 0;
+  `;
 };
 
 buttonCreate.addEventListener("click", renderDiv);
+buttonDestroy.addEventListener("click", destroyBoxes);
